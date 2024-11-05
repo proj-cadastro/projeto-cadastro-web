a1a025998a701c1e76d39d8e7ca0080bccd8d99a
erick-sts <erick.sts.eo@gmail.com>
2024-11-04 22:15:39 -0300

    ✨ Routes

---
+++ src/app/registerProfessors/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterProfessors() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1>Criar pagina de registro de professores</h1>
+      </div>
+    );
+  }
+  
---
+++ src/app/reportCurses/page.tsx
@@ -0,0 +1,7 @@
+export default function ReposrtCurses() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1>Criar pagina de relatorio de cursos</h1>
+    </div>
+  );
+}
