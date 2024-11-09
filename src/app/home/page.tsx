5d1c0948c8431a3124bf317b7fd5aee99e887423
erick-sts <erick.sts.eo@gmail.com>
2024-11-08 21:23:43 -0300

    ✨ Chart

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,22 @@
+"use client";
 import Footer from "@/components/organisms/Footer";
-import { Container } from "@mui/material";
+import { Card, Container } from "@mui/material";
+import Chart from "@/components/molecules/Chart";
+import { data, options } from "../../utils/mockChart";
 
 export default function Home() {
-    return (
-      <Container>
-
-
-
-        <Footer />
-      </Container>
-    );
-  }
-  
+  return (
+    <Container>
+      {/* navbar */}
+      <Card sx={{ mt: 8, mb: 5 }}></Card>
+      <Card sx={{ padding: 2, top: 4 }}>
+        <Chart
+          title="Distribuição de Professores"
+          data={data}
+          options={options}
+        />
+      </Card>
+      <Footer />
+    </Container>
+  );
+}
