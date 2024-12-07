3efb76378a997e6790d3678f9317b2e660805e11
erick-sts <erick.sts.eo@gmail.com>
2024-12-07 10:15:47 -0300

    ✨ Feat heat map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
 import "./globals.css";
 
 const theme = createTheme({
@@ -20,8 +21,28 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isProduction = process.env.NODE_ENV === "production";
+
   return (
     <html lang="pt-br">
+      <Head>
+        {isProduction && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:5234028,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
+        )}
+      </Head>
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
